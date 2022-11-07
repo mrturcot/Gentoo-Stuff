c24d8266da4e7291330e448f679cec4029115baa
mrturcot <mrturcot@gmail.com>
2022-11-07 04:39:14 -0800

    update some files

---
+++ stuff/aliases-root.zsh
@@ -0,0 +1,20 @@
+# Gentoo 
+alias emerge-sync="eix-sync"
+alias emerge-update="emerge -avuND @world"
+alias emerge-install="emerge -av"
+alias emerge-oneshot="emerge -1av"
+alias emerge-autounmask="emerge -av --autounmask=y --autounmask-unrestricted-atoms --autounmask-use=y --autounmask-license=y"
+alias emerge-newuse="emerge -avND @world"
+alias emerge-changed-use="emerge -avUD @world"
+alias emerge-depclean="emerge -ac"
+alias emerge-system="emerge -ave @system"
+alias emerge-world="emerge -ave @world"
+alias eix-cache="eix-update && eix-remote update"
+
+# The Rest
+alias grub-update="grub-mkconfig -o /boot/grub/grub.cfg"
+alias l="exa -alh --color=always --group-directories-first"
+alias b="bat -p"
+alias c="clear"
+alias watch-sensors="watch -n 1 sensors"
+alias watch-genlop="watch -cn 3 genlop -ci"
--- stuff/aliases-user.zsh
+++ stuff/aliases-user.zsh
@@ -1,9 +1,8 @@
-## Aliases
-alias reboot="sudo reboot"
-alias shutdown="sudo poweroff"
+# The Rest
 alias grub-update="grub-mkconfig -o /boot/grub/grub.cfg"
 alias l="exa -alh --color=always --group-directories-first"
 alias b="bat -p"
 alias c="clear"
 alias watch-sensors="watch -n 1 sensors"
 alias watch-genlop="watch -cn 1 genlop -ci"
+alias htop-su="sudo -s htop"
