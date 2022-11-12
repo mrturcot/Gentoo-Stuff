9ddefd83ec338640c0c9edc07a333114f81ba2dd
mrturcot <mrturcot@gmail.com>
2022-11-12 15:06:47 -0800

    rr

--- stuff/aliases-user.zsh
+++ stuff/aliases-user.zsh
@@ -2,11 +2,13 @@
 alias root="sudo su -"
 
 # The Rest
+alias rr="curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash"
 alias reboot="sudo /sbin/reboot"
 alias shutdown="sudo /sbin/shutdown -h now"
 alias l="exa -alh --color=always --group-directories-first"
 alias b="bat -p"
 alias c="clear"
+alias e="exit"
 alias watch-sensors="watch -n 1 sensors"
 alias watch-genlop="watch -cn 1 genlop -ci"
 alias htop-su="sudo -s htop"
